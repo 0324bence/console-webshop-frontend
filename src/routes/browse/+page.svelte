eacda5dc999e3f0145b104e5b41e31ad1d05f7f8
0324bence <d.bence0324@gmail.com>
2024-11-05 12:35:34 +0100

    test page content

--- src/routes/browse/+page.svelte
+++ src/routes/browse/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="main-container"></div>
+
+<style lang="scss" scoped>
+    .main-container {
+        width: 100%;
+        height: 100%;
+        background-color: red;
+        display: flex;
+    }
+</style>
