1833c4cc4b5788ff4bc1dad5731cf8676f69aabb
0324bence <d.bence0324@gmail.com>
2024-11-11 14:06:52 +0100

    add desing to user page

--- src/routes/main/profile/+page.svelte
+++ src/routes/main/profile/+page.svelte
@@ -4,26 +4,34 @@
 
 <div class="main-container">
     <div id="profile-container">
-        <div id="profile">
-            <div class="pic">
-                <UserPlaceholder />
+        <!-- <div id="profile"> -->
+        <div class="pic">
+            <UserPlaceholder />
+        </div>
+        <div class="infos">
+            <h1>Username</h1>
+            <div class="row">
+                <span>Csatlakozott:</span>
+                <span>2040. 01. 01.</span>
             </div>
-            <div class="infos">
-                <h1>Username</h1>
-                <div class="row">
-                    <span>Csatlakozott:</span>
-                    <span>2040. 01. 01.</span>
-                </div>
-                <div class="row">
-                    <span>Hirdetések:</span>
-                    <span>5</span>
-                </div>
-                <div class="row">
-                    <span>Értékelés:</span>
-                    <span>5/5</span>
-                </div>
+            <div class="row">
+                <span>Hirdetések:</span>
+                <span>5</span>
+            </div>
+            <div class="row">
+                <span>Értékelés:</span>
+                <span>5/5</span>
             </div>
         </div>
+        <div class="bio">
+            <p>
+                Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit sint et repellendus
+                aliquid doloremque explicabo sit ipsa ex, quo labore maiores non quidem quas dolor eum quibusdam facere
+                accusantium animi laudantium corporis? Officia eveniet recusandae alias in, maiores, error eaque sequi
+                consectetur, repellat iusto facere ipsa soluta sunt assumenda! A!
+            </p>
+        </div>
+        <!-- </div> -->
     </div>
     <div id="adverts"></div>
 </div>
@@ -40,7 +48,16 @@
             height: 40vh;
             display: flex;
             align-items: center;
-            padding-left: 5rem;
+            justify-content: center;
+            gap: 2rem;
+            padding: 0 10rem 0 10rem;
+            .pic {
+                height: 50%;
+            }
+
+            .infos {
+                width: 25%;
+            }
         }
     }
 </style>
