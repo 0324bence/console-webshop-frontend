4fff2966e40123f9b6efc81dc9940e47548a4b1d
0324bence <d.bence0324@gmail.com>
2024-11-25 11:31:15 +0100

    create layout of main page

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,10 +1,24 @@
-<div class="main-container"></div>
+<div class="main-container">
+    <div id="filter-container"></div>
+    <div id="sorting-container"></div>
+    <div id="advert-container"></div>
+</div>
 
 <style lang="scss" scoped>
     .main-container {
         width: 100%;
         height: 100%;
-        background-color: red;
         display: flex;
+        display: grid;
+        grid-template-columns: 3fr 17fr;
+        grid-template-rows: 1fr 15fr;
+
+        #filter-container {
+            grid-row: span 2;
+        }
+
+        div {
+            border: 1px solid black;
+        }
     }
 </style>
