ed3955bf39c87514d38bb7643f052290e38a86e2
0324bence <d.bence0324@gmail.com>
2024-12-03 12:34:50 +0100

    Start work on properly typing adverts

--- src/routes/(main)/(browse)/+layout.server.ts
+++ src/routes/(main)/(browse)/+layout.server.ts
@@ -1,14 +1,35 @@
 import { error, redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 import apiPath from "$lib/apiPath";
-import type { User } from "$lib/types";
+import type { LocalAdvert, Picture } from "$lib/types";
 
 export const load: LayoutServerLoad = async ({ cookies, url }) => {
     const advertsReq = await fetch(`${apiPath}/adverts${url.search}`);
     if (!advertsReq.ok) {
         return error(advertsReq.status, advertsReq.statusText);
     }
-    const adverts = await advertsReq.json();
+    const advertsRes = await advertsReq.json();
+    const adverts: LocalAdvert[] = advertsRes.items;
+    for (const advert of adverts) {
+        const picturesReq = await fetch(`${apiPath}/adverts/${advert.id}/pictures`);
+        if (picturesReq.ok) {
+            const pictures: Picture[] = await picturesReq.json();
+            advert.pictures = pictures;
+            advert.mainPicture = pictures.find(p => p.isPriority === 1) || pictures[0];
+        } else {
+            /*Todo convert nincs kép to base64*/
+            advert.pictures = [
+                {
+                    id: 0,
+                    data: "",
+                    description: "Nincs megadva kép",
+                    advertId: advert.id,
+                    isPriority: 1
+                }
+            ];
+            advert.mainPicture = advert.pictures[0];
+        }
+    }
 
     //states
     const activeStates = url.searchParams.get("stateIds")?.split(",").map(Number) || [];
