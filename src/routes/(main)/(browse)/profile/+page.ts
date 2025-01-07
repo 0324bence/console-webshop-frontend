cdfbf2696ac4c4fe9cca56f92954dae8f0eea008
0324bence <d.bence0324@gmail.com>
2025-01-07 14:50:13 +0100

    redirect to auth page when accessing profile without login

--- src/routes/(main)/(browse)/profile/+page.ts
+++ src/routes/(main)/(browse)/profile/+page.ts
@@ -3,5 +3,6 @@ import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ parent }) => {
     const { ownUser } = await parent();
+    if (ownUser === null) return redirect(303, "/auth");
     return redirect(303, `/profile/${ownUser.id}`);
 };
