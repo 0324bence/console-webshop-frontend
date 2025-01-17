13feed36c3a8ab6133c3993cec9265f0666b92fa
0324bence <d.bence0324@gmail.com>
2025-01-17 10:52:43 +0100

    Add descriptions

--- src/routes/(main)/advert/[id]/+page.svelte
+++ src/routes/(main)/advert/[id]/+page.svelte
@@ -23,6 +23,11 @@
                     class="image"
                     style={`background-image: url('data:image/jpeg;base64,${selectedImage.data}');`}
                 ></div>
+                <div id="description">
+                    <p>
+                        {selectedImage.description}
+                    </p>
+                </div>
             </div>
         </div>
         <div id="data-container">
@@ -52,42 +57,7 @@
     </div>
     <div id="description-container">
         <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente quis, quia harum sequi odio minima enim,
-            placeat dolore saepe rerum architecto officia dolorum fugiat! Nesciunt quos odit delectus, nemo animi sunt
-            veritatis eligendi porro aperiam eaque, nisi expedita neque excepturi inventore, voluptatem iusto!
-            Dignissimos quidem eum quis quam? Suscipit ut exercitationem praesentium modi amet cum provident. Maiores
-            atque qui reprehenderit omnis placeat? Quod animi quidem, nisi reprehenderit numquam labore doloribus,
-            nihil, eveniet libero est quisquam. Repellendus, labore expedita animi et ipsum illo? Obcaecati, tempore
-            quas quam, quae quisquam delectus ullam nam veniam eius, asperiores sint autem est vitae cupiditate
-            explicabo incidunt excepturi ipsam ratione quos optio. Sint suscipit consequatur illum in ea sunt voluptatem
-            deserunt debitis quo perferendis similique animi, pariatur hic, labore corporis laudantium aperiam qui.
-            Error, animi iure a, obcaecati nihil cupiditate quaerat tenetur et possimus, inventore ipsam pariatur natus
-            dolore est. Laborum sequi quae ipsa similique eius blanditiis esse alias fuga deserunt incidunt? Cumque
-            earum quas consequatur eligendi laudantium alias nemo sint officia vero? Repellat, iure, optio nulla
-            perspiciatis eum culpa eveniet, illo dolor recusandae soluta laudantium. Et accusamus quam, enim nobis,
-            accusantium praesentium mollitia atque in voluptatibus soluta dolores explicabo laboriosam dolorem adipisci
-            repellendus, suscipit illo. Modi porro nostrum dolorem dolore doloremque, ipsum voluptatem eveniet veritatis
-            maiores a pariatur. Minus, architecto sunt in corporis impedit autem, odio asperiores consectetur, totam
-            provident necessitatibus aperiam saepe doloribus dolorem nihil officiis. Quo quisquam dolorem natus, beatae
-            voluptatibus labore atque laboriosam quae cumque assumenda minima ducimus ipsum sit molestiae tempora
-            voluptate impedit officiis expedita at excepturi vitae perspiciatis iste maxime eos. Corporis harum neque
-            id, mollitia ratione qui cupiditate voluptatum, natus vitae provident, deserunt iste tempore doloremque
-            eaque? Eligendi facere debitis aperiam inventore eum molestias numquam, amet quas architecto repellat est
-            reprehenderit suscipit magnam laborum repudiandae sunt, dolor odit vitae eius ipsam voluptates esse quod
-            veniam nobis. Magnam nam facilis, incidunt doloribus totam unde consequatur deserunt soluta velit harum
-            libero eum similique tenetur rem inventore earum accusantium ipsa vitae a corrupti ducimus aut, dolores
-            perferendis? Cumque iure consequatur earum nisi nihil sequi corrupti ex ipsum labore dolorum, sapiente fuga
-            autem doloremque voluptatum. Assumenda, nam consectetur voluptatem iusto cupiditate laboriosam cum maxime,
-            quae, praesentium harum repellendus magnam minus! Sit enim maxime nihil magni. Illo maxime sunt soluta quis?
-            Voluptatibus assumenda quo quisquam atque iusto eveniet qui ex, harum veritatis cum dignissimos animi nulla!
-            Modi laboriosam cupiditate unde fugit corrupti doloremque quas harum! Iure recusandae repellendus, odit
-            dolorum fuga quae? Error alias rem corrupti minus nesciunt saepe sequi labore aliquam ab, eum cumque quia
-            amet id ullam nostrum quis, veniam itaque cupiditate sed minima eligendi? Nisi explicabo animi perspiciatis.
-            Placeat quam consectetur architecto vitae eum doloribus, itaque dolores at dignissimos est velit nam nulla
-            tempore cupiditate sequi sit ea voluptatem obcaecati nihil maiores eveniet culpa. Sequi earum necessitatibus
-            temporibus possimus vero rerum officia nemo? Eos quasi quo at quas ipsa. Blanditiis amet impedit eaque rerum
-            aperiam! Laudantium, adipisci fugiat. Quae odit, sint placeat doloribus, neque dicta laborum, veritatis
-            necessitatibus fugit commodi ipsam.
+            {data.advert.description}
         </p>
     </div>
 </div>
@@ -152,6 +122,7 @@
 
                 #main-container {
                     display: flex;
+                    flex-direction: column;
                     justify-content: center;
                     align-items: center;
                     padding: 1rem;
